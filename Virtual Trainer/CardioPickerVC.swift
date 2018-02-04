f6591771d2519f05cdc3128860f379d5f4ba303b
Farbod909 <rafezyfarbod@gmail.com>
2018-02-03 20:55:59 -0800

    Create two view controllers

---
+++ Virtual Trainer/CardioPickerVC.swift
@@ -0,0 +1,23 @@
+//
+//  CardioPickerVC.swift
+//  Virtual Trainer
+//
+//  Created by Farbod Rafezy on 2/3/18.
+//  Copyright © 2018 Farbod Rafezy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CardioPickerVC : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
